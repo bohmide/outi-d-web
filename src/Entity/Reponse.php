c6853aa6d5901555db9b0c7d4b916c93dbb01708
TakwaMejri <mejria742@gmail.com>
2025-02-21 07:50:26 +0100

    suivi noté 1 cours CRUD

--- src/Entity/Reponse.php
+++ src/Entity/Reponse.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use App\Repository\ReponseRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 #[ORM\Entity(repositoryClass: ReponseRepository::class)]
 class Reponse
@@ -14,9 +16,13 @@ class Reponse
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "La réponse ne peut pas être vide.")]
+    #[Assert\Length(min: 3, max: 255, minMessage: "La réponse doit comporter au moins {{ limit }} caractères.",
+     maxMessage: "La réponse ne peut pas dépasser {{ limit }} caractères.")]
     private ?string $reponse = null;
 
     #[ORM\Column]
+    #[Assert\NotNull(message: "La valeur de la réponse doit être spécifiée.")]
     private ?bool $isCorrect = null;
 
     #[ORM\ManyToOne(inversedBy: 'reponse')]
