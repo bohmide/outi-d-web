07718e7addeae8cd02cbcc97048f0de68d6121c5
bohmide <ahmedbousnina02@gmail.com>
2025-02-24 14:39:05 +0100

    participate + date 3 entity

--- migrations/Version20250224133407.php
+++ migrations/Version20250224133407.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250221081455 extends AbstractMigration
+final class Version20250224133407 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20250221081455 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE chapitre ADD CONSTRAINT FK_8C62B025853CD175 FOREIGN KEY (quiz_id) REFERENCES quiz (id) ON DELETE SET NULL');
+        $this->addSql('ALTER TABLE evenements ADD date_creation DATE NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE chapitre DROP FOREIGN KEY FK_8C62B025853CD175');
+        $this->addSql('ALTER TABLE evenements DROP date_creation');
     }
 }
